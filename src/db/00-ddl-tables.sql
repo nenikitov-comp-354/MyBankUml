4d55954ff663f7d79c11212e5982a74c0d2e51c7
nenikitov <nenikitov@mail.ru>
2025-11-06 13:41:27 -0500

    fix(db): initialization script

--- src/db/00-ddl-tables.sql
+++ src/db/00-ddl-tables.sql
@@ -1,39 +1,39 @@
 create table bank(
-    id serial primary key,
+    id bigint primary key generated always as identity,
     name varchar(128) not null
 );
 
 create table branch(
     id serial primary key,
     address varchar(128) not null,
-    bank serial references bank(id) not null
+    bank bigint references bank(id) not null
 );
 
 create domain social_insurance_number as char(11) check(value ~ '^\d{3}-\d{3}-\d{3}$');
-create domain phone as vachar(15) check(value ~ '^\+[1-9]\d{1,14}$');
-create domain email as vachar(256) check(value ~ '^.*@.*$');
+create domain phone as varchar(15) check(value ~ '^\+[1-9]\d{1,14}$');
+create domain email as varchar(256) check(value ~ '^.*@.*$');
 
 create table customer(
-    id serial primary key,
+    id bigint primary key generated always as identity,
     first_name varchar(64) not null,
     last_name varchar(64) not null,
     date_of_birth date not null,
     social_insurance_number social_insurance_number not null unique,
     phone phone,
-    email email not null,
+    email email not null
 );
 
 create table account(
-    id serial primary key,
+    id bigint primary key generated always as identity,
     name varchar(64) not null,
     is_locked boolean not null default false,
-    customer serial references customer(id) not null
+    customer bigint references customer(id) not null
 );
 
 create table transaction(
-    id serial primary key,
-    from serial references account(id) not null,
-    to serial references account(id) not null,
-    amount money not null check (money > 0),
+    id bigint primary key generated always as identity,
+    source bigint references account(id) not null,
+    destination bigint references account(id) not null,
+    amount money not null check (amount > 0::money),
     date date not null
 );
