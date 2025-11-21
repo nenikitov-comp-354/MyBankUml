4698ebdcc18a65fe5409857bbd19122240562b41
nenikitov <nenikitov@mail.ru>
2025-11-21 17:02:40 -0500

    feat: add junit as dependency

---
+++ src/bank/db/_TestBank.java
@@ -0,0 +1,12 @@
+package bank.db;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+final class _TestBank {
+    @Test
+    void testMath() {
+        assertEquals(4, 2 + 2);
+    }
+}
