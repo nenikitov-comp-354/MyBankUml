1170310e515860f3b0934f3366042e7edb97c334
nenikitov <nenikitov@mail.ru>
2025-11-21 18:49:00 -0500

    chore: find a better formatter

--- src/bank/db/_TestBank.java
+++ src/bank/db/_TestBank.java
@@ -1,12 +1,37 @@
 package bank.db;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 final class _TestBank {
-    @Test
-    void testMath() {
-        assertEquals(4, 2 + 2);
+
+    @ParameterizedTest
+    @CsvSource({ "1, 'First World Bank'", "2, 'The Big Bank'" })
+    void testConstructorValid(int id, String name) {
+        Bank bank = new Bank(id, name);
+        assertEquals(id, bank.getId());
+        assertEquals(name, bank.getName());
+        assertEquals(new ArrayList<>(), bank.getBranches());
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        {
+            "-3, 'First World Bank', 'Id `-3` is not a valid SQL id (must be > 0)'",
+            "3,  '   ',              'Name `   ` is blank or starts and ends with trailing spaces'",
+        }
+    )
+    void testConstructorInvalid(int id, String name, String error) {
+        Exception e = assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+                new Bank(id, name);
+            }
+        );
+        assertEquals(error, e.getMessage());
     }
 }
