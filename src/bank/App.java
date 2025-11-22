49d63d7a83ff7792641b63cce3cf2a0580edbd57
Soultimus <needtosendn0w@gmail.com>
2025-11-22 16:12:22 -0500

    Make app start at login page

--- src/bank/App.java
+++ src/bank/App.java
@@ -1,6 +1,10 @@
 package bank;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -16,7 +20,7 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
         // TODO: Use the DB connection
         // BankDb db = new BankDb("localhost", Optional.empty(), "bank", "admin", Optional.of("admin"));
 
@@ -41,18 +45,11 @@ public class App extends Application {
         // Transaction transaction = new Transaction(1, transactionInfo);
         // account1.addTransaction(transaction);
         //
-        // System.out.println(transaction);
+        // System.out.println(transaction);;
 
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label(
-            "Hello, JavaFX " +
-            javafxVersion +
-            ", running on Java " +
-            javaVersion +
-            "."
-        );
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        System.out.println(getClass().getClassLoader().getResource("fxml/LogInPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LogInPage.fxml"));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
