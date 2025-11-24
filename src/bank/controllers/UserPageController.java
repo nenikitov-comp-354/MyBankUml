925b7087cc92dde34906118d6ffdd8c36873536a
Soultimus <needtosendn0w@gmail.com>
2025-11-24 15:27:22 -0500

    Display name on account page

--- src/bank/controllers/UserPageController.java
+++ src/bank/controllers/UserPageController.java
@@ -4,28 +4,35 @@ import java.io.IOException;
 import java.util.List;
 
 import bank.db.Account;
+import bank.db.Customer;
 import bank.util.SceneManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class UserPageController {
 
     @FXML
     private VBox accountHolderVBox;
+    @FXML
+    private Text userNameText;
 
     private SceneManager sceneManager = SceneManager.getInstance();
 
     public void initialize() {
         try {
-            List<Account> accounts = sceneManager.getCustomer().getAccounts();
+            Customer currentCustomer = sceneManager.getCustomer();
+            userNameText.setText("Welcome, " + currentCustomer.getFirstName() + "!");
+            List<Account> accounts = currentCustomer.getAccounts();
 
             for (Account a : accounts) {
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + a.getClass().getSimpleName() + ".fxml"));
                 Parent card = loader.load();
                 accountHolderVBox.getChildren().add(card);
+                VBox.setMargin(card, new javafx.geometry.Insets(15));
             }
         } catch (IOException e) {
             e.printStackTrace();
