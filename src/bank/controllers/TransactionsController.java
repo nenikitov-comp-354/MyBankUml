fba866fa86c1cb7f349c8d9e146a4d4266bff4c4
ayasophya <jadekamsing@gmail.com>
2025-11-23 20:27:30 -0500

    1st draft chequing send money pop up

---
+++ src/bank/controllers/TransactionsController.java
@@ -0,0 +1,32 @@
+package bank.controllers;
+
+import bank.util.SceneManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TransactionsController {
+
+    @FXML
+    private Button savingsButton;
+    @FXML
+    private Button contactsButton;
+    @FXML
+    private Button cancelButton;
+
+    private SceneManager sceneManager = SceneManager.getInstance();
+
+    @FXML
+    private void handleSavings(ActionEvent event) {
+        sceneManager.switchScene("/fxml/SendMoneySavings.fxml", savingsButton);
+    }
+    @FXML
+    private void handleContacts(ActionEvent event) {
+        sceneManager.switchScene("/fxml/SendMoneyContacts.fxml", contactsButton);
+    }
+
+    @FXML
+    private void handleCancel(ActionEvent event) {
+        sceneManager.switchScene("/fxml/UserPage.fxml", cancelButton);
+    }
+}
